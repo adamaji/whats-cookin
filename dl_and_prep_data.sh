ebea6a0400ab11362326ed83db8a2c846c931a33
Adam Aji <adam.m.aji@lmco.com>
2016-12-21 00:46:40 -0500

    Add preparation script and test video list

---
+++ dl_and_prep_data.sh
@@ -0,0 +1,60 @@
+# Downloads and prepares the videos and accompanying data 
+# from the What's Cookin release for alignment
+# Usage: dl_and_prep_data.sh [path to initial list of videos]
+# Prereqs: youtube-dl, ffmpeg
+
+LISTPATH=$1
+UNIQID=$(cat $LISTPATH | cut -d , -f 1 | sort | uniq)
+
+WORKING_DIR="./data"
+SEGMENT_LENGTH=8 # in seconds
+
+mkdir $WORKING_DIR
+
+echo "Downloading videos..."
+mkdir $WORKING_DIR/videos
+mkdir $WORKING_DIR/frames
+for id in $UNIQID; do
+  # save a temporary full version of the video
+  youtube-dl -f mp4 -o $WORKING_DIR/videos/$id.full.mp4 https://youtube.com/watch?v=$id;
+
+  # cut out the relevant segments and extract the frames
+  for entry in $(cat $LISTPATH | grep $id ); do
+    s=$(echo $entry | cut -d , -f 6); # grab seconds value
+    ms=$(echo $entry | cut -d , -f 2); # grab milliseconds value
+    echo $s | xargs -I {} \
+    ffmpeg \
+    -ss {} \
+    -i $WORKING_DIR/videos/$id.full.mp4 \
+    -t $SEGMENT_LENGTH -c copy $WORKING_DIR/videos/$id.$ms.mp4;
+    mkdir $WORKING_DIR/frames/$id.$ms;
+    ffmpeg \
+    -i $WORKING_DIR/videos/$id.$ms.mp4 \
+    -vf "select=not(mod(n\,10)), scale=-1:256" \
+    -vsync vfr -q:v 2 -y $WORKING_DIR/frames/$id.$ms/frame_%04d.jpg;
+  done
+
+  # remove the temp full video
+  rm $WORKING_DIR/videos/$id.full.mp4;
+done
+
+echo "Downloading video descriptions..."
+mkdir $WORKING_DIR/descriptions
+for id in $UNIQID; do
+youtube-dl \
+--skip-download \
+--write-description \
+-o "$WORKING_DIR/descriptions/%(id)s.%(ext)s" \
+https://youtube.com/watch?v=$id
+done
+
+echo "Downloading speech transcripts..."
+mkdir $WORKING_DIR/transcripts
+for id in $UNIQID; do
+  youtube-dl \
+  --skip-download \
+  --write-auto-sub \
+  --sub-format ttml \
+  -o $WORKING_DIR/transcripts/$id.ttml \
+  https://youtube.com/watch?v=$id;
+done
